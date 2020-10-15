3757b950ff4c67bb6725dbb33d28e5172bca3895
Ruslan Riznyk <44848472+roolrd@users.noreply.github.com>
2020-10-15 18:34:08 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+
+variable "server_name" {
+  description = "Name of Webserver"
+  type        = string
+  default     = "ROOL-web-"
+}
+variable "server_size" {
+  description = "Size of Webserver"
+  type        = string
+  default     = "t2.micro"
+}
